95fb4ae15d72ef940accfb292c310bbbcb9d0da0
Fernando <fquesada@ncq.co.cr>
2022-10-01 08:28:28 -0600

    Creación de pantalla de login con video de fondo

--- app/src/main/java/com/fquesada/apprestaurante/ui/MainActivity.kt
+++ app/src/main/java/com/fquesada/apprestaurante/ui/MainActivity.kt
@@ -1,16 +1,55 @@
 package com.fquesada.apprestaurante.ui
 
+import android.content.Context
+import android.net.Uri
 import android.os.Bundle
+import android.view.ViewGroup.LayoutParams.MATCH_PARENT
+import android.widget.FrameLayout
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusManager
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.modifier.modifierLocalOf
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
 import com.fquesada.apprestaurante.ui.theme.ApprestauranteTheme
+import com.fquesada.apprestaurante.R
+import com.fquesada.apprestaurante.ui.theme.Shapes
+import com.google.accompanist.insets.ProvideWindowInsets
+import com.google.accompanist.insets.navigationBarsWithImePadding
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.Player
+import com.google.android.exoplayer2.ui.AspectRatioFrameLayout.RESIZE_MODE_ZOOM
+import com.google.android.exoplayer2.ui.StyledPlayerView
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,22 +61,151 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    Login(getVideoUri())
+                }
+            }
+        }
+    }
+
+    private fun getVideoUri(): Uri {
+        val rawId = resources.getIdentifier("fondologin","raw",packageName)
+        val videoUri = "android.resource://$packageName/$rawId"
+        return Uri.parse(videoUri)
+    }
+}
+
+private fun Context.buildExoPlayer(uri:Uri) =
+    ExoPlayer.Builder(this).build().apply {
+        setMediaItem(MediaItem.fromUri(uri))
+        repeatMode = Player.REPEAT_MODE_ALL
+        playWhenReady = true
+        prepare()
+    }
+
+private fun Context.buildPlayerView(exoPlayer: ExoPlayer)=
+    StyledPlayerView(this).apply {
+        player = exoPlayer
+        layoutParams = FrameLayout.LayoutParams(MATCH_PARENT, MATCH_PARENT)
+        useController = false
+        resizeMode = RESIZE_MODE_ZOOM
+    }
+
+
+
+@Composable
+fun Login(videoUri: Uri) {
+    val context = LocalContext.current
+    val claveFocusRequester = FocusRequester()
+    val focusManager: FocusManager = LocalFocusManager.current
+    val exoPlayer = remember {
+        context.buildExoPlayer(videoUri)
+    }
+
+    DisposableEffect(
+        AndroidView(
+            factory = { it.buildPlayerView(exoPlayer)},
+            modifier = Modifier.fillMaxSize()
+        )
+    ){
+        onDispose {
+            exoPlayer.release()
+        }
+    }
+
+    ProvideWindowInsets {
+        Column(
+            Modifier
+                .navigationBarsWithImePadding()
+                .padding(24.dp)
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.spacedBy(16.dp, alignment = Alignment.Bottom),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.logorestaurante),
+                contentDescription = null,
+                Modifier.size(250.dp),
+                tint = Color.White
+            )
+            TextInput(InputType.Usuario, keyboardActions = KeyboardActions(onNext = {
+                claveFocusRequester.requestFocus()
+            }))
+
+            TextInput(InputType.Clave, keyboardActions = KeyboardActions(onDone = {
+                focusManager.clearFocus()
+            }), focusRequester = claveFocusRequester)
+            Button(
+                shape = RoundedCornerShape(20.dp),
+                onClick = { /*TODO*/ },
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(text = "Ingreso", Modifier.padding(vertical = 8.dp))
+
+            }
+            Divider(
+                color = Color.White.copy(alpha = 0.3f),
+                thickness = 1.dp,
+                modifier = Modifier.padding(top = 48.dp)
+            )
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Text(text = "Ingreso al sistema", color = Color.White)
+                TextButton(onClick = { /*TODO*/ }) {
+                    Text(text = "Regitro de comandas")
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+sealed class InputType(
+    val label: String,
+    val icon: ImageVector,
+    val keyboardOptions: KeyboardOptions,
+    val visualTransformation: VisualTransformation
+){
+    object Usuario: InputType(
+        label = "Usuario",
+        icon = Icons.Default.Person,
+        keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next),
+        visualTransformation = VisualTransformation.None
+    )
+
+    object Clave : InputType(
+        label = "Clave",
+        icon = Icons.Default.Lock,
+        keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done, keyboardType = KeyboardType.Password),
+        visualTransformation = PasswordVisualTransformation()
+
+    )
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    ApprestauranteTheme {
-        Greeting("Android")
+fun TextInput(inputType: InputType,
+              focusRequester: FocusRequester? = null,
+              keyboardActions: KeyboardActions
+              ){
+    var value by remember {
+        mutableStateOf("")
     }
+    TextField(value = value, onValueChange = {
+        value = it
+    },
+        modifier = Modifier
+            .fillMaxWidth()
+            .focusRequester(focusRequester ?: FocusRequester()),
+        leadingIcon = { Icon(imageVector = inputType.icon, null)},
+        label = { Text(text = inputType.label)},
+        shape = RoundedCornerShape(20.dp),
+        colors = TextFieldDefaults.textFieldColors(
+            backgroundColor = Color.Black,
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent,
+            disabledIndicatorColor = Color.Transparent
+        ),
+        singleLine = true,
+        keyboardOptions = inputType.keyboardOptions,
+        visualTransformation = inputType.visualTransformation,
+        keyboardActions = keyboardActions
+
+    )
 }
